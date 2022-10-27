c5bf141fc3fccbb8d4f4b5b39ef619aaf9b839bc
Chris Eadie <caeadie@hotmail.com>
2022-10-27 19:10:44 +0100

    working api reading from database

--- Api Code/index.js
+++ Api Code/index.js
@@ -1,25 +1,58 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+  
+const db = getFirestore();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/api/GetAll",async (req,res) => {
+
+    const projectRef = db.collection('projects');
+    const snapshot = await projectRef.get();
+
+    const dataList = new Array();
+    snapshot.forEach(doc => {
+        dataList.push(doc.data())
+    });
+
     return res.json({
         sucess:true,
-        data:{
-            // put some data here to return
+        content:{
+            data:dataList
         }
     })
 })
 
 app.get("/api/GetById/:id",async (req,res) => {
-    const requestId = req.params.id;
+    
+    const projectId = req.params.id;
 
     // access database and retrieve document based on ID
+    const datasetRef = db.collection('datasets');
+    const snapshot = await datasetRef.where("projectId", "==",projectId).get();
+
+    const dataList = new Array();
+    snapshot.forEach(doc => {
+        dataList.push(doc.data())
+    });
 
     return res.json({
         sucess:true,
-        data:{
-            // return data requested from database
+        content:{
+            data:dataList
         }
     })
 })
@@ -30,10 +63,22 @@ app.post("/api/Search",async (req,res) => {
 
     // parse search arguments into firebase query rules
 
+    let projectRef = db.collection('projects');
+
+    if(searchArgs?.name){
+        projectRef = projectRef.where('name', '>=', searchArgs?.name).where('name', '<=', searchArgs?.name + '~');
+    }
+    const snapshot = await projectRef.get();
+
+    const dataList = new Array();
+    snapshot.forEach(doc => {
+        dataList.push(doc.data())
+    });
+
     return res.json({
         sucess:true,
-        data:{
-            // return data requested from database
+        content:{
+            data:dataList
         }
     })
 })
